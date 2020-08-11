c085952261f495fd11259b90c4e6e88566c6f494
yasayas <yasa.social@gmail.com>
2020-08-11 09:22:56 +0800

    crud done

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+class PostController extends Controller
+{
+    public function create() {
+        return view('post.create');
+    }
+
+    public function store(Request $request) {
+        // dd($request->all());
+        $request->validate([
+            "title" => "required|unique:posts",
+            "body" => "required"
+            ]);
+
+        $query = DB::table('posts')->insert([
+            "title" => $request["title"],
+            "body" => $request["body"]
+        ]);
+
+        return redirect('/posts/create');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/master', function () {
+    return view('adminlte.master');
+});
+
+Route::get('/', function () {
+    return view('items.index');
+});
+
+Route::get('/data-tables', function () {
+    return view('items.data-tables');
+});
+
+Route::get('/home', 'HomeController@home');
+Route::get('/register', 'AuthController@register');
+Route::post('/welcome', 'AuthController@welcome');
+
+Route::get('/posts/create', 'PostController@create');
+Route::post('/posts', 'PostController@store');
+
+
+Route::get('/pertanyaan', 'PertanyaanController@index');
+Route::get('/pertanyaan/create', 'PertanyaanController@create');
+Route::post('/pertanyaan', 'PertanyaanController@store');
+Route::get('/pertanyaan/{pertanyaan_id}', 'PertanyaanController@show');
+Route::get('/pertanyaan/{pertanyaan_id}/edit', 'PertanyaanController@edit');
+Route::put('/pertanyaan/{pertanyaan_id}', 'PertanyaanController@update');
+Route::delete('/pertanyaan/{pertanyaan_id}', 'PertanyaanController@destroy');
